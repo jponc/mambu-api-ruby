75c9fa389211951fa5d4ca04c05790ead9130f0f
Jakub Kubacki <kubacki.jk@gmail.com>
2015-06-12 18:06:12 +0200

    Api model #find_all spec
    [#96657970]

--- spec/mambu/api_model_spec.rb
+++ spec/mambu/api_model_spec.rb
@@ -9,4 +9,27 @@ describe Mambu::ApiModel do
       expect(described_class.endpoint(client)).to eq 'https://tenant.sandbox.mambu.com/api/objects'
     end
   end
+
+  describe "#find_all" do
+    context "endpoint exists" do
+      it "returns array of resource", :vcr do
+        allow(described_class).to receive(:api_uri).and_return('loanproducts')
+        expect(described_class.find_all(client)).to be_kind_of Array
+      end
+    end
+
+    context "endpoint doesn't exist" do
+    end
+  end
+
+  describe "#find" do
+    context "resource exist" do
+    end
+
+    context "resource doesn't exist" do
+    end
+
+    context "endpoint doesn't exist" do
+    end
+  end
 end
