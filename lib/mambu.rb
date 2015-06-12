bdad0b4afcdc61c76adc641390daf2e09ec8bff9
Jakub Kubacki <kubacki.jk@gmail.com>
2015-06-12 14:03:19 +0200

    Add loan product model
    [#96657970]

--- lib/mambu.rb
+++ lib/mambu.rb
@@ -4,6 +4,7 @@ require "json"
 require "mambu/version"
 require "mambu/client"
 require "mambu/api_model"
+require "mambu/loan_product"
 
 module Mambu
   # Your code goes here...
---
+++ lib/mambu/loan_product.rb
@@ -0,0 +1,7 @@
+module Mambu
+  class LoanProduct < ApiModel
+    def self.api_uri
+      'loanproducts'
+    end
+  end
+end
