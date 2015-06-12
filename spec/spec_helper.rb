f6836fbe81696abc6b51622393f97a883c36bc06
Jakub Kubacki <kubacki.jk@gmail.com>
2015-06-12 17:27:42 +0200

    Setup vcr
    [#96657970]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,21 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'mambu'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/fixtures/cassettes"
+  c.hook_into :webmock
+end
+
+RSpec.configure do |c|
+  c.around(:each, :vcr) do |example|
+    name = example
+           .metadata[:full_description]
+           .split(/\s+/, 2)
+           .join("/")
+           .underscore
+           .gsub(%r{[^\w\/]+}, "_")
+    options = example.metadata.slice(:record, :match_requests_on).except(:example_group)
+    VCR.use_cassette(name, options) { example.call }
+  end
+end
