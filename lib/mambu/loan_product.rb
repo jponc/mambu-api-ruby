a9292aac5114b4c154442e11e020d705f7ae79f3
Jakub Kubacki <kubacki.jk@gmail.com>
2015-06-17 12:43:50 +0200

    Remove #api_uri from Loan Product. It is generated in api model
    [#96657970]

--- lib/mambu/loan_product.rb
+++ lib/mambu/loan_product.rb
@@ -2,10 +2,6 @@ module Mambu
   class LoanProduct < ApiModel
     attr_accessor :loan_fees
 
-    def self.api_uri
-      'loanproducts'
-    end
-
     def loan_fees=(data)
       @loan_fees = data.map { |hash_fee| Mambu::LoanFee.new(hash_fee) }
     end
