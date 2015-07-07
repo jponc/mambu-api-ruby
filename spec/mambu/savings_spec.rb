d4f25d11868f7c3d2daebf79a2abe0e73faa8847
Jacek Kubiak <jacek.kubiak@netguru.pl>
2015-07-07 16:33:43 +0200

    Add test for Savings class

---
+++ spec/mambu/savings_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe Mambu::Savings do
+  let(:connection) { Mambu::Connection.new('user', 'password', 'tenant.sandbox') }
+  let(:account_id) { 'ABCD123' }
+
+  context 'creating deposit' do
+    let(:data) { { type: 'DEPOSIT', amount: 222 } }
+    let(:invalid_data) { data.merge(method: 'name') }
+    let(:invalid_data_2) { data.merge('AccountName': 'Test Name') }
+    let(:valid_transaction) do
+      described_class.create_deposit(account_id, connection, data)
+    end
+    let(:invalid_transaction) do
+      described_class.create_deposit(account_id, connection, invalid_data)
+    end
+    let(:invalid_transaction_2) do
+      described_class.create_deposit(account_id, connection, invalid_data_2)
+    end
+
+    context 'successful transaction' do
+      it 'returns a Mambu Savings object', :vcr do
+        expect(valid_transaction).to be_kind_of described_class
+      end
+
+      it 'values of returned object match those from api', :vcr do
+        object = valid_transaction
+        expect(object.amount).to eq 222
+        expect(object.type).to eq 'DEPOSIT'
+      end
+    end
+
+    context 'unsuccessful transaction' do
+      it 'returns an error', :vcr do
+        expect { invalid_transaction }.to raise_error Mambu::Error
+      end
+
+      it 'provides an error description', :vcr do
+        expect { invalid_transaction }.to raise_error do |error|
+          error.status == "INVALID_TRANSACTION_TYPE_ID"
+        end
+      end
+
+      it 'doesnt allow any unnecessary attributes', :vcr do
+        expect { invalid_transaction_2 }.to raise_error do |error|
+          error.code == 108
+        end
+      end
+    end
+  end
+end
